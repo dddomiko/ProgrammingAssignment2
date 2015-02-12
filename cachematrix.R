b806eae5d01a1f46065f46cd81ae2441e0899083
dddomiko <dominik-w-koch@web.de>
2015-02-13 00:14:23 +0100

    Fixed Typo & Testing
    <<- instead of <-
    
    Added testing section

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,10 @@
 ## makeCacheMatrix: This function creates a special "matrix", which is really a 
 ## list containing a function to
 
-## set the value of the matrix
-## get the value of the matrix
-## set the inverse of the matrix
-## get the inverse of the matrix
+## set: set the value of the matrix
+## get: get the value of the matrix
+## setinv: set the inverse of the matrix
+## getinv: get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
   # set: update matrix und reset inverse matrix variable
   set <- function(y){
     x <<- y
-    inv <- NULL
+    inv <<- NULL
   }
   
   # get: get the matrix
@@ -36,7 +36,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cachSolve: This function calculates the inverse of the "matrix" created with
+## cacheSolve: This function calculates the inverse of the "matrix" created with
 ## the above funkction. However, it first checks to see if the inverse has 
 ## already been calculated. If so, it gets the inverse from the cache and skips
 ## the computation. Otherwise, it calculates the inverse of the matrix and 
@@ -55,10 +55,57 @@ cacheSolve <- function(x, ...) {
   # get matrix 
   data <- x$get()
   # calculate inverse of the matrix
-  inv <- solve(x, ...)
+  inv <- solve(data, ...)
   # update cache
   x$setinv(inv)
   
   ## Return a matrix that is the inverse of 'x'
   inv
 }
+
+### testing ####################################################################
+
+## initialize "special" matrix
+# test <- makeCacheMatrix(matrix(c(2,3,4,5),nrow = 2))
+
+## get matrix
+# test$get()
+#
+# >      [,1] [,2]
+# > [1,]    2    4
+# > [2,]    3    5
+
+## get inverse of the matrix
+# test$getinv()
+# 
+# > NULL
+
+## calculate inverse matrix
+# cacheSolve(test)
+# 
+# >      [,1] [,2]
+# > [1,] -2.5    2
+# > [2,]  1.5   -1
+
+## calculate inverse matrix again
+# cacheSolve(test)
+# 
+# > getting cached inverse of the matrix
+# >      [,1] [,2]
+# > [1,] -2.5    2
+# > [2,]  1.5   -1
+
+## get inverse of the matrix
+# test$getinv()
+# 
+# >      [,1] [,2]
+# > [1,] -2.5    2
+# > [2,]  1.5   -1
+
+## set new matrix
+# test$set(matrix(c(3,3,5,6),nrow = 2))
+
+## check: cache deleted
+# test$getinv()
+# 
+# > NULL
